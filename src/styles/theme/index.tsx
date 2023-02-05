1bcea564f0adf37233b808a7bc2c694722383db2
riancarlosdev <riancontatoprofissional@gmail.com>
2023-02-05 10:48:13 -0300

    clean

--- src/styles/theme/index.tsx
+++ src/styles/theme/index.tsx
@@ -1,11 +1,1 @@
-export const colors = {
-  black: "#0e1215",
-  black2: "#11161b",
-  black3: "#151b22",
-  black4: "#18212b",
-  black_active: "#000",
-  white: "#a4a4a4",
-  white_active: "#fff",
-  blue: "#0384a5",
-  blue_active: "#016a84",
-};
+export const colors = {};
